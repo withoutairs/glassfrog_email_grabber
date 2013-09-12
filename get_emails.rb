5566b9204ef4e14d0dca3dfb7b1dd68a705a3043
Kevin McHugh <kmchugh@backstopsolutions.com>
2013-09-12 15:00:51 -0500

    Map circle names to ids.

--- get_emails.rb
+++ get_emails.rb
@@ -10,10 +10,12 @@ end
 def glassfrog_key
 	ENV['GLASSFROG_KEY']
 end
-
-circles = get "circle"
-x = Nokogiri::XML.parse(circles).xpath("circles/circle").map do |circle|
+xml_circles = get("circle")
+circles = Nokogiri::XML.parse(xml_circles).xpath("circles/circle")
+circle_hashes = circles.map do |circle|
 	{id: circle.xpath('id').text, name: circle.xpath('name').text}
 end
-
-puts x
+names_to_ids = {}
+circle_hashes.each { |circle| names_to_ids[circle[:name]] = circle[:id]}
+	
+puts names_to_ids
