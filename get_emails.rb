9e38d9b5937a9af8541af790c925745659e74c3e
Kevin McHugh <kmchugh@backstopsolutions.com>
2013-09-12 16:25:21 -0500

    Don't get all circles if you don't have to.

--- get_emails.rb
+++ get_emails.rb
@@ -19,20 +19,21 @@ def glassfrog_uri; 'https://glassfrog.holacracy.org/api/v2/'; end
 def glassfrog_key; ENV['GLASSFROG_KEY']; end
 def target_circle; ARGV.first; end
 
-circles_xml = get("circle")
-circles = Nokogiri::XML.parse(circles_xml).xpath("circles/circle")
-circle_hashes = circles.map do |circle|
-	{id: circle.xpath('id').text, name: circle.xpath('short-name').text}
-end
-names_to_ids = {}
-circle_hashes.each { |circle| names_to_ids[circle[:name]] = circle[:id]}
-
 if default_roles.keys.include? target_circle
 	puts get_emails get default_roles[target_circle]
-elsif target_circle 
-	circle_id = names_to_ids[target_circle]
-	puts get_emails get "circle/#{circle_id}/mailing_list"
 else
-	puts "please specify a circle from the below:"
-	puts circle_hashes.map {|circle| circle[:name]}
+	circles_xml = get("circle")
+	circles = Nokogiri::XML.parse(circles_xml).xpath("circles/circle")
+	circle_hashes = circles.map do |circle|
+		{id: circle.xpath('id').text, name: circle.xpath('short-name').text}
+	end
+	names_to_ids = {}
+	circle_hashes.each { |circle| names_to_ids[circle[:name]] = circle[:id]}
+	if target_circle 
+		circle_id = names_to_ids[target_circle]
+		puts get_emails get "circle/#{circle_id}/mailing_list"
+	else
+		puts "please specify a circle from the below:"
+		puts circle_hashes.map {|circle| circle[:name]}
+	end
 end
