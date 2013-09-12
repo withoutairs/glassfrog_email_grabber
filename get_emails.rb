3a354a437867cd42946d0344d78bfb38e997d959
Kevin McHugh <kmchugh@backstopsolutions.com>
2013-09-12 15:13:37 -0500

    Get & print all email addresses for a circle.

--- get_emails.rb
+++ get_emails.rb
@@ -7,22 +7,18 @@ def get method
 	URI.parse("#{GLASSFROG_URI}#{method}.xml?api_key=#{glassfrog_key}").read
 end
 
-def glassfrog_key
-	ENV['GLASSFROG_KEY']
-end
+def glassfrog_key; ENV['GLASSFROG_KEY']; end
+def target_circle; ARGV.first; end
 
-def target_circle
-	ARGV.first
-end
-
-xml_circles = get("circle")
-circles = Nokogiri::XML.parse(xml_circles).xpath("circles/circle")
+circles_xml = get("circle")
+circles = Nokogiri::XML.parse(circles_xml).xpath("circles/circle")
 circle_hashes = circles.map do |circle|
 	{id: circle.xpath('id').text, name: circle.xpath('name').text}
 end
 names_to_ids = {}
 circle_hashes.each { |circle| names_to_ids[circle[:name]] = circle[:id]}
 	
-puts names_to_ids
 circle_id = names_to_ids[target_circle]
-puts get "circle/#{circle_id}/mailing_list"
+circle_xml = get "circle/#{circle_id}/mailing_list"
+people = Nokogiri::XML.parse(circle_xml).xpath("people/person/email")
+puts people.map {|person| "#{person.text}"}
