c32760d0a345ea7f85d22bea7e9f95dae762b9d7
Kevin McHugh <kmchugh@backstopsolutions.com>
2013-09-12 15:58:15 -0500

    Support for Lead Links, Rep Links, Secretaries, Facilitators

--- README.md
+++ README.md
@@ -2,5 +2,6 @@ USAGE:
 * bundle install
 * export GLASSFROG_KEY=$YOUR_GLASSFROG_KEY
 * ruby get_emails "$CIRCLE_NAME_YOU_CARE_ABOUT", where that is the short name. Usually that's the name, sometimes there's a different name in italicized parens.
+** or "Faciliators", "Lead Links", "Rep Links", or "Secretaries"
 
-KNOWN BUGS:
+I didn't really test this
--- get_emails.rb
+++ get_emails.rb
@@ -5,6 +5,11 @@ def get method
 	URI.parse("#{glassfrog_uri}#{method}.xml?api_key=#{glassfrog_key}").read
 end
 
+def get_emails circle_xml
+	emails = Nokogiri::XML.parse(circle_xml).xpath("people/person/email")
+	emails.map {|email| "#{email.text}"}
+end
+
 def glassfrog_uri; 'https://glassfrog.holacracy.org/api/v2/'; end
 def glassfrog_key; ENV['GLASSFROG_KEY']; end
 def target_circle; ARGV.first; end
@@ -17,11 +22,18 @@ end
 names_to_ids = {}
 circle_hashes.each { |circle| names_to_ids[circle[:name]] = circle[:id]}
 
-if (target_circle) 
+case target_circle
+when "Facilitators"
+	puts get_emails get "facilitators"
+when "Lead Links"
+	puts get_emails get "lead_links"
+when "Secretaries"
+	puts get_emails get "secretaries"
+when "Rep Links"
+	puts get_emails get "rep_links"
+when String
 	circle_id = names_to_ids[target_circle]
-	circle_xml = get "circle/#{circle_id}/mailing_list"
-	emails = Nokogiri::XML.parse(circle_xml).xpath("people/person/email")
-	puts emails.map {|email| "#{email.text}"}
+	puts get_emails get "circle/#{circle_id}/mailing_list"
 else
 	puts "please specify a circle from the below:"
 	puts circle_hashes.map {|circle| circle[:name]}
