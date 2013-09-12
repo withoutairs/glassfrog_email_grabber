c4d6a578a7d9b57c32174a1d8b43a39bf5e58504
Kevin McHugh <kmchugh@backstopsolutions.com>
2013-09-12 15:05:27 -0500

    Get the circle specified by the first CL arg.

--- get_emails.rb
+++ get_emails.rb
@@ -10,6 +10,11 @@ end
 def glassfrog_key
 	ENV['GLASSFROG_KEY']
 end
+
+def target_circle
+	ARGV.first
+end
+
 xml_circles = get("circle")
 circles = Nokogiri::XML.parse(xml_circles).xpath("circles/circle")
 circle_hashes = circles.map do |circle|
@@ -19,3 +24,5 @@ names_to_ids = {}
 circle_hashes.each { |circle| names_to_ids[circle[:name]] = circle[:id]}
 	
 puts names_to_ids
+circle_id = names_to_ids[target_circle]
+puts get "circle/#{circle_id}/mailing_list"
